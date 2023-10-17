d7f6541e73ebe3c9c1cb0c8eef45cd3575b84ef2
bw060010 <122111677+bw060010@users.noreply.github.com>
2023-10-17 11:44:00 -0600

    - Test set dropdown

--- tests/test_set_dropdown.py
+++ tests/test_set_dropdown.py
@@ -4,7 +4,7 @@ from utils.driver import setup_driver, teardown_driver
 from utils.helpers import generate_random_string, take_screenshot
 
 # Define the test function
-def test_fill_fields():
+def test_set_dropdown():
     # Set up the driver
     driver = setup_driver()
 
@@ -15,15 +15,11 @@ def test_fill_fields():
     # Create an instance of the TutorialPage class
     tutorial_page = TutorialPage(driver)
 
-    # Fill the name field with a random string of length 10
-    tutorial_page.fill_name(generate_random_string(10))
-    # Fill the email field
-    tutorial_page.fill_email('avinash@qxf2.com')
-    # Fill the phone field
-    tutorial_page.fill_phone('9999999999')
+    # Fill the male gender in the dropdown
+    tutorial_page.click_gender('Male')
 
     # Take a screenshot and save it as 'screenshot.png'
-    take_screenshot(driver, 'screenshot.png')
+    take_screenshot(driver, 'screenshot_dropdown.png')
 
     # Tear down the driver
     teardown_driver(driver)
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,15 +1,46 @@
+# Import necessary libraries
 import random
 import string
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Function to generate a random string of a given length
 def generate_random_string(length):
+    """
+    Generate a random string of a given length.
+
+    Parameters:
+    length (int): The length of the string to generate.
+
+    Returns:
+    str: A random string of the specified length.
+    """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
+# Function to wait for an element to be present
 def wait_for_element(driver, by, value):
+    """
+    Wait for an element to be present on the page.
+
+    Parameters:
+    driver (WebDriver): The WebDriver instance to use.
+    by (By): The method to locate the element.
+    value (str): The value for the method to locate the element.
+
+    Returns:
+    WebElement: The WebElement once it is located.
+    """
     wait = WebDriverWait(driver, 10)
     return wait.until(EC.presence_of_element_located((by, value)))
 
+# Function to take a screenshot
 def take_screenshot(driver, filename):
+    """
+    Take a screenshot of the current page.
+
+    Parameters:
+    driver (WebDriver): The WebDriver instance to use.
+    filename (str): The name of the file to save the screenshot as.
+    """
     driver.save_screenshot(filename)
